4bb960c5eab77037982cd26013516299692f291c
kaightasa <kei17.kaight084@gmail.com>
2022-01-07 08:56:56 +0900

    removed some files

--- test_script/oncpu_graph_generator.sh
+++ test_script/oncpu_graph_generator.sh
@@ -5,14 +5,15 @@ cd ~/ros2-galactic-analysis/test_script
 PID=`ps aux | grep pubsub/talker | head -1 | awk '{print $2}'`
 
 
-sudo perf record -F 999 -g -p $PID --call-graph dwarf -- sleep 30
+sudo perf record -F 999 -g -p $PID --call-graph dwarf -- sleep 120
 
-sudo perf script > stack.txt 
+sudo perf script > stack.txt
 
 sudo cat stack.txt | ../../FlameGraph/stackcollapse-perf.pl > out.perf-folded
-sudo ../../FlameGraph/flamegraph.pl out.perf-folded > ../result/publisher/svg/1MB/publisher_stack_trace_1MB.svg
+mkdir -p ../result/publisher/svg/1KB
+sudo ../../FlameGraph/flamegraph.pl out.perf-folded > ../result/publisher/svg/30Hz/publisher_stack_trace_1KB_16sub.svg
 
-rm -rf perf.data stack.txt out.perf-folded
+rm -rf perf.data perf.data.old stack.txt out.perf-folded
 
 
 
